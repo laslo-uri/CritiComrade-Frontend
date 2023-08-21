a28b6cf29c026c90ff6fb6428db82a13ee871a8d
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2023-08-22 01:28:26 +0200

    Logo and Color Palette Updates
    Improved navbar with a logo and selected primary colors for the website's visual identity.

--- criticomrade/src/app/app.module.ts
+++ criticomrade/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
